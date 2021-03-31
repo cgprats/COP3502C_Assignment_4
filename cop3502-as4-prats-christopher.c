2ee53cc113e9dadd29329013f92bfd706cad98b7
Christopher Prats <christopherprats@gmail.com>
2021-03-31 12:18:40 -0400

    Added Required Structs

--- cop3502-as4-prats-christopher.c
+++ cop3502-as4-prats-christopher.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Structs
+// This is the Item Node Struct
+struct item_node_struct {
+	char name[32];
+	int count;
+	struct item_node_struct *left, *right;
+};
+typedef struct item_node_struct item_node;
+
+//This is the Tree Name Node Struct
+struct tree_name_node_struct {
+	char treeName[32];
+	struct tree_name_node_struct *left, *right;
+	item_node *theTree;
+};
+typedef struct tree_name_node_struct tree_name_node;
+
+// Function Prototypes
+
+// Constructor Prototypes
+
+// Destructor Prototypes
+
 // The Main Function
 int main() {
 	return 0;
